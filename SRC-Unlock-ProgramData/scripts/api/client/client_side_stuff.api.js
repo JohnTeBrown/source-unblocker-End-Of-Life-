96525a0e198c7c0425bd620e4cb624e84aa0af40
Dusk & Rain Studios <116376838+JohnTeBrown@users.noreply.github.com>
2023-02-19 12:54:23 -0500

    Delete SRC-Unlock-ProgramData directory

--- SRC-Unlock-ProgramData/scripts/api/client/client_side_stuff.api.js
+++
@@ -1,9 +0,0 @@
-// client side code shall remain empty for now
-// I'm leaving this empty as I cannot easily manipulate things that are client side
-// makin the server connect with the client is hard, having the full version of node running would be great
-// but sadly the linux beta is blocked
-// Regards, Impulse admin.
-
-
-
-// currentlty a WIP and not in use, though if you have the linux beta install coffeescript and the clientside test script will autocompile and run!
